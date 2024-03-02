b40ecd98f4d76c23e2c1559284a1df3ea4b9a5c9
muratseyfi <muratseyfitr@gmail.com>
2024-03-03 00:56:50 +0300

    Product Quantity On Cart

--- cart/cart.py
+++ cart/cart.py
@@ -17,14 +17,16 @@ class Cart():
         self.cart = cart
 
     
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
 
         # Logic
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price: ': str(product.price) }
+            #self.cart[product_id] = {'price: ': str(product.price) }
+            self.cart[product_id] = int(product_qty)
         
         self.session.modified = True
     
@@ -40,3 +42,7 @@ class Cart():
         
         # Return those looked up products
         return products
+    
+    def cart_quants(self):
+        quantities = self.cart
+        return quantities
