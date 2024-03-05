a68e8f7e9ad2921948685ade0cb077a6ba93320c
muratseyfi <muratseyfitr@gmail.com>
2024-03-05 12:24:03 +0300

    Update Password

--- store/views.py
+++ store/views.py
@@ -4,8 +4,32 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm, UpdateUserForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
 from django import forms
+
+def update_password(request):
+    if request.user.is_authenticated:
+        current_user = request.user
+        # Did they fill out the form
+        if request.method == 'POST':
+            form = ChangePasswordForm(current_user, request.POST)
+            # Is the form valid
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Your Password Has Been Updated, Please Log In Again...")
+                #login(request, current_user)
+                return redirect('login')
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(request, error)
+                    return redirect('update_password')
+        else:
+            form = ChangePasswordForm(current_user)
+            return render(request, "update_password.html", {'form': form})
+    else:
+        messages.success(request, "You Must Be LoggedIn To View That Page!")
+        return redirect('home')
+
 
 def update_user(request):
 
